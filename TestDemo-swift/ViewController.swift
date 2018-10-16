0aa2d9a1217c8e5dace9516d4a98b454034c3ccf
lignpeng <vip_goldenapple@126.com>
2018-10-16 17:09:31 +0800

    创建工程

---
+++ TestDemo-swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestDemo-swift
+//
+//  Created by lignpeng on 2018/10/16.
+//  Copyright © 2018年 com.lignpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white;
+        self.title = "TestDemo-swift"
+        NSLog("---------")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
