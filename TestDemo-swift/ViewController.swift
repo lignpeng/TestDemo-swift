3f17389a006d0e39f29bb0569e7ff3825be1c109
lignpeng <vip_goldenapple@126.com>
2019-08-29 15:17:57 +0800

    建立工程

--- TestDemo-swift/ViewController.swift
+++ TestDemo-swift/ViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    lazy var tableView:UITableView = {
+        let tableview = UITableView(frame: UIScreen.main.bounds)
+        tableview.delegate = self
+        tableview.dataSource = self;
+        tableview.tableFooterView = UIView()
+        return tableview
+    }()
+    
+    lazy var dataSurce: [String] = {
+        let dataSurce = ["1"]
+        return dataSurce
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white;
-        self.title = "TestDemo-swift"
-        NSLog("---------")
+        self.title = "TestDemo-Swift"
+        self.view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSurce.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "cellID"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        if (cell == nil) {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+        }
+        cell?.textLabel?.text = self.dataSurce[indexPath.row]
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
---
+++ TestDemo-swift/view/Popview.swift
@@ -0,0 +1,13 @@
+//
+//  Popview.swift
+//  TestDemo-swift
+//
+//  Created by lignpeng on 2019/3/21.
+//  Copyright © 2019年 com.lignpeng. All rights reserved.
+//
+
+import UIKit
+
+class Popview: UIView {
+    
+}
